06868d67093cead75b4ec4e43b44491bae8aa583
brianbushree <bcbushree@dons.usfca.edu>
2017-06-19 14:50:26 -0700

    Updated README.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # pillars
-App used to visualize call hierarchy structure of java projects, built on Electron.
+App used to visualize class structure of Java projects, built on Electron.
+
+## Purpose/Goal
+* Allow developers to easily visualize small or large scale Java projects by looking at interaction between classes using method call-hierarchy. 
+
+## End-User Scenario
+1. Take in user input, a directory with .java files.
+2. For each .java file, run 'javap -p', parse the output, store the methods' attributes.
+3. Display classes/methods structure for user interaction.
+4. Once a method is selected, visualize Call Heirarchy of a selected method using [call-hierarchy-printer](https://github.com/pbadenski/call-hierarchy-printer) and D3.
 
 ## Plan of Execution
-1. Take in user input, a directory with .java files.
-2. For each .java file, run 'javap -l', parse the output, store the methods' attributes and line number range.
-3. Display methods for interaction.
-4. Once a method is selected, visualize Call Heirarchy of a selected method using [call-hierarchy-printer](https://github.com/pbadenski/call-hierarchy-printer).
+1. Open directory of .java/.class files.
+2. Run 'javap -p' on each class file. Parse the output into array of class objects with array of methods.
+3. For each class, for each method, run call-hierarchy-printer to see its callees. Store a list of callees in each method.
+4. Use class/method data to run an interactive D3 hierarchy visualization.
--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const dialog = electron.dialog
 
 const path = require('path')
 const url = require('url')
+
+const exec = require('child_process').exec
+const fs = require('fs')
 
 let mainWindow
 
 function createWindow() {
 
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false, titleBarStyle: 'hidden-inset'})
+  mainWindow = new BrowserWindow({
+      width: 800, height: 600,
+      frame: false, titleBarStyle: 'hidden-inset' })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -18,11 +24,42 @@ function createWindow() {
   }))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
+  loadProject()
+}
+
+function loadProject() {
+  openDirectory()
+}
+
+function openDirectory() {
+
+  let dir = dialog.showOpenDialog({
+      properties: [ 'openDirectory' ] })
+
+  if (dir.length > 0) {
+    dir.forEach(function(d) {
+      fs.readdir(d, (err, files) => {
+        files.forEach(file => {
+          if (file.split('.').pop() == 'class') {
+            let f = (d + '/' + file).replace(/ /g, '\\ ')
+            exec('javap -p ' + f,
+              function (error, stdout, stderr){
+                console.log('Output -> ' + stdout);
+                if(error !== null){
+                  console.log("Error -> "+error);
+                }
+            })
+          }
+        })
+      })
+    })
+  }
 }
 
 app.on('ready', createWindow)
